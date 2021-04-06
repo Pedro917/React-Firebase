fa176a522d88f0fb75b715ff62ff732a94a0ab75
Pedro917 <pedrobarbosa.first@gmail.com>
2021-04-06 13:19:42 -0300

    Questions form ok

--- src/views/Questoes.jsx
+++ src/views/Questoes.jsx
@@ -2,12 +2,11 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router";
 import Card from 'react-bootstrap/Card';
-// import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
-import Table from 'react-bootstrap/Table';
 import { GiHamburgerMenu } from "react-icons/gi";
-import { FaTimes } from "react-icons/fa";
+import { FaTimes, FaPlus } from "react-icons/fa";
 
 class Questoes extends Component {
     constructor(){
@@ -20,24 +19,34 @@ class Questoes extends Component {
     render(){
         return (
             <div>
+                <h6 className="text-secondary mb-4">GERENCIAMENTO</h6>
                 <Card>
-                <Card.Header>Featured</Card.Header>
-                <Card.Body>
-                    <Table responsive="md">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Questão</th>
-                                <th>Ações</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <td><GiHamburgerMenu /></td>
-                            <td>Teste ?</td>
-                            <td><Button variant="danger"><FaTimes /></Button></td>
-                        </tbody>
-                    </Table>
-                </Card.Body>
+                    <Card.Header>QUESTÕES PARA FEEDBACK</Card.Header>
+                    <Card.Body>
+                        <div className="d-flex mb-3">
+                            <Button variant="light" disabled><GiHamburgerMenu /></Button>
+                            <Form.Control type="text" placeholder="Readonly input here..." className="mx-3" readOnly />
+                            <Button variant="danger"><FaTimes /></Button>
+                        </div>
+                        <div className="d-flex mb-3">
+                            <Button variant="light" disabled><GiHamburgerMenu /></Button>
+                            <Form.Control type="text" placeholder="Readonly input here..." className="mx-3" readOnly />
+                            <Button variant="danger"><FaTimes /></Button>
+                        </div>
+                        <hr className="my-3"/>
+
+                        <Form>
+                            <Form.Row > 
+                                <Col lg={10}>
+                                    <Form.Control placeholder="Digite a nova pergunta a ser adicionada" />
+                                </Col>
+                                <Col lg={2}>
+                                    <Button variant="success" className="d-flex align-items-center"><FaPlus className="mr-2" /> Adicionar</Button>
+                                </Col>
+                            </Form.Row>
+                        </Form>
+
+                    </Card.Body>
                 </Card>
 
             </div>
